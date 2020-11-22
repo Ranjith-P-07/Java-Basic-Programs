080db7941ca62f27534d7882863edfef9eded693
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-22 19:38:57 +0530

    To Print Ascii values from 0-127

---
+++ AsciiDemo/src/Ranjith/AsciiDemo.java
@@ -0,0 +1,14 @@
+package Ranjith;
+
+public class AsciiDemo {
+
+	public static void main(String[] args) {
+		
+		for(int num=0;num<=127;num++)
+		{
+			System.out.printf("%d : %c \n",num,num);
+		}
+
+	}
+
+}
